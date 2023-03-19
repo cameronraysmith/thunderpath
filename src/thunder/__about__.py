21beceee0882d45b929fa0973cf507d50500a46e
Cameron Smith <cameron.ray.smith@gmail.com>
2023-03-19 01:17:17 -0400

    add package

---
+++ src/thunder/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2023-present Cameron Smith
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+__version__ = "0.0.0"
---
+++ src/thunder/cli/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2023-present biotheorylab
+#
+# SPDX-License-Identifier: MIT
+import click
+
+from thunder.__about__ import __version__
+
+
+@click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
+@click.version_option(version=__version__, prog_name="thunder")
+def thunder():
+    click.echo("make pyro thunder!")
