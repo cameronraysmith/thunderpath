a71fffecec6e6def3a7554889c719984c17b97c1
Cameron Smith <cameron.ray.smith@gmail.com>
2023-05-07 01:20:07 -0400

    test vae model

--- tests/models/test_vae.py
+++ tests/models/test_vae.py
@@ -29,3 +29,41 @@ def test_decoder(vae):
     assert x_loc.size() == (32, 100)
     assert x_scale.size() == (32, 100)
 
+
+@pytest.mark.parametrize("batch_size", [1, 16, 32, 64])
+def test_guide(vae, batch_size):
+    x = torch.randn(batch_size, 100)
+    with torch.no_grad():
+        # Use pyro.poutine.trace to record the execution trace while running the guide function
+        with pyro.poutine.trace() as guide_trace:
+            vae.guide(x)
+
+    # Access the recorded trace from the guide_trace object
+    trace = guide_trace.trace
+    assert "latent" in {site["name"] for site in trace.nodes.values()}
+
+
+@pytest.mark.parametrize("batch_size", [1, 16, 32, 64])
+def test_model(vae, batch_size):
+    x = torch.randn(batch_size, 100)
+    with torch.no_grad():
+        # Use poutine.trace to get the trace of the model function
+        trace = pyro.poutine.trace(vae.model).get_trace(x)
+    assert "latent" in {site["name"] for site in trace.nodes.values()}
+    assert "obs" in {site["name"] for site in trace.nodes.values()}
+
+
+def training_step(self, batch, batch_idx):
+    x, _ = batch
+    pyro.clear_param_store()
+    svi_step = pyro.infer.SVI(self.model, self.guide, self.optim, self.elbo)
+    loss = torch.tensor(svi_step.step(x))
+    self.log("train_loss", loss)
+    return loss
+
+
+def test_configure_optimizers(vae):
+    optimizer = vae.configure_optimizers()
+    assert isinstance(optimizer, pyro.optim.PyroOptim)
+
+
