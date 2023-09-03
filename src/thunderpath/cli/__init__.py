1b29c0db70215cb9fc3b8abdce69c72e69ec2d9e
Cameron Smith <cameron.ray.smith@gmail.com>
2023-09-03 14:23:12 -0400

    fix(docs): thunderpath rename
    - express intent to support any widely utilized probabilistic programming/modeling tool
    - initial implementations will likely span pyro/numpyro

--- src/thunderpath/cli/__init__.py
+++ src/thunderpath/cli/__init__.py
@@ -9,4 +9,4 @@ from thunderpath.__about__ import __version__
 @click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
 @click.version_option(version=__version__, prog_name="thunderpath")
 def main():
-    click.echo("make pyro thunderpath!")
+    click.echo("hello thunderpath")
